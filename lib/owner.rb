d6513b44b1f497d2d75857a36ddb85ace6367c83
Jack Madden <jck13mad@gmail.com>
2020-04-02 21:22:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,7 +4,7 @@ class Owner
   attr_reader :species
   
   @@all = []
-  @pets = {:cats => [], :dogs => []}
+  @@pets = {:cats => [], :dogs => []}
   
   def initialize(species)
     @species = species
