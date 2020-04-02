cb3664417a45ea21c3e048923ce664f06eabb5a8
Jack Madden <jck13mad@gmail.com>
2020-04-02 21:48:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,13 @@ class Dog
   # code goes here
     attr_accessor :mood, :owner
     attr_reader :name
+    
+    @@all = []
   
   def initialize(name, owner)
     @owner = owner
     @mood = "nervous"
+    @@all << self
   end
   
 end
